850744fc23571b10636e6e97f6d6697eaebb6260
Hassan <hassansmallah@gmail.com>
2023-11-05 14:25:15 +0100

    polls app + lib multiple-choice-list-filter

--- chooseColumns/chooseColumns/settings.py
+++ chooseColumns/chooseColumns/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_admin_multiple_choice_list_filter',
+    'polls',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
